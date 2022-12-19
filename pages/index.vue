ae44a39c9b603b5582e28641d5a0d47116f47870
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-12-20 00:40:21 +0600

    layout done

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,489 @@
 <template>
-  <Tutorial/>
+	<div class="">
+		<section class="pt-4 pb-4">
+			<div class="container">
+				<div class="row mt-2 mb-2">
+					<div class="col-lg-4 col-md-4">
+						<h3 class="fw-bolder">Courses</h3>
+					</div>
+
+					<div class="col-lg-4 col-md-4 ms-auto">
+						<select class="form-select" aria-label="Default select example">
+							<option selected>Open this select menu</option>
+							<option value="1">One</option>
+							<option value="2">Two</option>
+							<option value="3">Three</option>
+						</select>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-md-6 col-12 mt-4">
+						<div class="card">
+							<img src="~/assets/images/js.jpg" class="card-img-top" alt="..." />
+							<i class="bi bi-bookmark card-bookmark"></i>
+							<div class="card-body pb-0">
+								<div class="tutor-course-ratings">
+									<div class="tutor-ratings">
+										<div class="tutor-ratings">
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+										</div>
+										<div class="tutor-ratings-average">5.0</div>
+										<div class="tutor-ratings-count">(7)</div>
+									</div>
+								</div>
+								<h5 class="card-title mt-3 mb-3">Card title</h5>
+								<div class="tutor-meta mb-3">
+									<div class="d-inline">
+										<span class="tutor-meta-icon tutor-icon-user-line" area-hidden="true"><i class="bi bi-person"></i></span>
+										<span class="tutor-meta-value">815</span>
+									</div>
+
+									<div class="d-inline p-2">
+										<span class="tutor-meta-icon tutor-icon-clock-line ml-3" area-hidden="true"><i class="bi bi-clock"></i></span>
+										<span class="tutor-meta-value"> 2h 23m</span>
+									</div>
+								</div>
+
+								<div class="tutor-meta">
+									<div class="d-inline">
+										<div class="tutor-avatar d-inline">
+											<img src="https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=Gazi+Moshiul" alt="" srcset="" />
+										</div>
+									</div>
+
+									<div class="d-inline">
+										By
+										<a href="#">Polash Mahmud</a>
+
+										In
+										<a href="#">পলাশ মাহমুদ</a>
+									</div>
+								</div>
+							</div>
+							<hr class="border-1 mb-2" />
+							<a href="single-page.html" class="btn-tutor">Enroll Course</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 col-12 mt-4">
+						<div class="card">
+							<img src="~/assets/images/js.jpg" class="card-img-top" alt="..." />
+							<i class="bi bi-bookmark card-bookmark"></i>
+							<div class="card-body pb-0">
+								<div class="tutor-course-ratings">
+									<div class="tutor-ratings">
+										<div class="tutor-ratings">
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+										</div>
+										<div class="tutor-ratings-average">5.0</div>
+										<div class="tutor-ratings-count">(7)</div>
+									</div>
+								</div>
+								<h5 class="card-title mt-3 mb-3">Card title</h5>
+								<div class="tutor-meta mb-3">
+									<div class="d-inline">
+										<span class="tutor-meta-icon tutor-icon-user-line" area-hidden="true"><i class="bi bi-person"></i></span>
+										<span class="tutor-meta-value">815</span>
+									</div>
+
+									<div class="d-inline p-2">
+										<span class="tutor-meta-icon tutor-icon-clock-line ml-3" area-hidden="true"><i class="bi bi-clock"></i></span>
+										<span class="tutor-meta-value"> 2h 23m</span>
+									</div>
+								</div>
+
+								<div class="tutor-meta">
+									<div class="d-inline">
+										<div class="tutor-avatar d-inline">
+											<img src="~/assets/images/user-profile-icon.jpg" alt="" srcset="" />
+										</div>
+									</div>
+
+									<div class="d-inline">
+										By
+										<a href="#">Polash Mahmud</a>
+
+										In
+										<a href="#">পলাশ মাহমুদ</a>
+									</div>
+								</div>
+							</div>
+							<hr class="border-1 mb-2" />
+							<a href="#" class="btn-tutor">Enroll Course</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 col-12 mt-4">
+						<div class="card">
+							<img src="~/assets/images/js.jpg" class="card-img-top" alt="..." />
+							<i class="bi bi-bookmark card-bookmark"></i>
+							<div class="card-body pb-0">
+								<div class="tutor-course-ratings">
+									<div class="tutor-ratings">
+										<div class="tutor-ratings">
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+										</div>
+										<div class="tutor-ratings-average">5.0</div>
+										<div class="tutor-ratings-count">(7)</div>
+									</div>
+								</div>
+								<h5 class="card-title mt-3 mb-3">Card title</h5>
+								<div class="tutor-meta mb-3">
+									<div class="d-inline">
+										<span class="tutor-meta-icon tutor-icon-user-line" area-hidden="true"><i class="bi bi-person"></i></span>
+										<span class="tutor-meta-value">815</span>
+									</div>
+
+									<div class="d-inline p-2">
+										<span class="tutor-meta-icon tutor-icon-clock-line ml-3" area-hidden="true"><i class="bi bi-clock"></i></span>
+										<span class="tutor-meta-value"> 2h 23m</span>
+									</div>
+								</div>
+
+								<div class="tutor-meta">
+									<div class="d-inline">
+										<div class="tutor-avatar d-inline">
+											<img src="~/assets/images/user-profile-icon.jpg" alt="" srcset="" />
+										</div>
+									</div>
+
+									<div class="d-inline">
+										By
+										<a href="#">Polash Mahmud</a>
+
+										In
+										<a href="#">পলাশ মাহমুদ</a>
+									</div>
+								</div>
+							</div>
+							<hr class="border-1 mb-2" />
+							<a href="#" class="btn-tutor">Enroll Course</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 col-12 mt-4">
+						<div class="card">
+							<img src="~/assets/images/js.jpg" class="card-img-top" alt="..." />
+							<i class="bi bi-bookmark card-bookmark"></i>
+							<div class="card-body pb-0">
+								<div class="tutor-course-ratings">
+									<div class="tutor-ratings">
+										<div class="tutor-ratings">
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+										</div>
+										<div class="tutor-ratings-average">5.0</div>
+										<div class="tutor-ratings-count">(7)</div>
+									</div>
+								</div>
+								<h5 class="card-title mt-3 mb-3">Card title</h5>
+								<div class="tutor-meta mb-3">
+									<div class="d-inline">
+										<span class="tutor-meta-icon tutor-icon-user-line" area-hidden="true"><i class="bi bi-person"></i></span>
+										<span class="tutor-meta-value">815</span>
+									</div>
+
+									<div class="d-inline p-2">
+										<span class="tutor-meta-icon tutor-icon-clock-line ml-3" area-hidden="true"><i class="bi bi-clock"></i></span>
+										<span class="tutor-meta-value"> 2h 23m</span>
+									</div>
+								</div>
+
+								<div class="tutor-meta">
+									<div class="d-inline">
+										<div class="tutor-avatar d-inline">
+											<img src="~/assets/images/user-profile-icon.jpg" alt="" srcset="" />
+										</div>
+									</div>
+
+									<div class="d-inline">
+										By
+										<a href="#">Polash Mahmud</a>
+
+										In
+										<a href="#">পলাশ মাহমুদ</a>
+									</div>
+								</div>
+							</div>
+							<hr class="border-1 mb-2" />
+							<a href="#" class="btn-tutor">Enroll Course</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 col-12 mt-4">
+						<div class="card">
+							<img src="~/assets/images/js.jpg" class="card-img-top" alt="..." />
+							<i class="bi bi-bookmark card-bookmark"></i>
+							<div class="card-body pb-0">
+								<div class="tutor-course-ratings">
+									<div class="tutor-ratings">
+										<div class="tutor-ratings">
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+										</div>
+										<div class="tutor-ratings-average">5.0</div>
+										<div class="tutor-ratings-count">(7)</div>
+									</div>
+								</div>
+								<h5 class="card-title mt-3 mb-3">Card title</h5>
+								<div class="tutor-meta mb-3">
+									<div class="d-inline">
+										<span class="tutor-meta-icon tutor-icon-user-line" area-hidden="true"><i class="bi bi-person"></i></span>
+										<span class="tutor-meta-value">815</span>
+									</div>
+
+									<div class="d-inline p-2">
+										<span class="tutor-meta-icon tutor-icon-clock-line ml-3" area-hidden="true"><i class="bi bi-clock"></i></span>
+										<span class="tutor-meta-value"> 2h 23m</span>
+									</div>
+								</div>
+
+								<div class="tutor-meta">
+									<div class="d-inline">
+										<div class="tutor-avatar d-inline">
+											<img src="~/assets/images/user-profile-icon.jpg" alt="" srcset="" />
+										</div>
+									</div>
+
+									<div class="d-inline">
+										By
+										<a href="#">Polash Mahmud</a>
+
+										In
+										<a href="#">পলাশ মাহমুদ</a>
+									</div>
+								</div>
+							</div>
+							<hr class="border-1 mb-2" />
+							<a href="#" class="btn-tutor">Enroll Course</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 col-12 mt-4">
+						<div class="card">
+							<img src="~/assets/images/js.jpg" class="card-img-top" alt="..." />
+							<i class="bi bi-bookmark card-bookmark"></i>
+							<div class="card-body pb-0">
+								<div class="tutor-course-ratings">
+									<div class="tutor-ratings">
+										<div class="tutor-ratings">
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+										</div>
+										<div class="tutor-ratings-average">5.0</div>
+										<div class="tutor-ratings-count">(7)</div>
+									</div>
+								</div>
+								<h5 class="card-title mt-3 mb-3">Card title</h5>
+								<div class="tutor-meta mb-3">
+									<div class="d-inline">
+										<span class="tutor-meta-icon tutor-icon-user-line" area-hidden="true"><i class="bi bi-person"></i></span>
+										<span class="tutor-meta-value">815</span>
+									</div>
+
+									<div class="d-inline p-2">
+										<span class="tutor-meta-icon tutor-icon-clock-line ml-3" area-hidden="true"><i class="bi bi-clock"></i></span>
+										<span class="tutor-meta-value"> 2h 23m</span>
+									</div>
+								</div>
+
+								<div class="tutor-meta">
+									<div class="d-inline">
+										<div class="tutor-avatar d-inline">
+											<img src="~/assets/images/user-profile-icon.jpg" alt="" srcset="" />
+										</div>
+									</div>
+
+									<div class="d-inline">
+										By
+										<a href="#">Polash Mahmud</a>
+
+										In
+										<a href="#">পলাশ মাহমুদ</a>
+									</div>
+								</div>
+							</div>
+							<hr class="border-1 mb-2" />
+							<a href="#" class="btn-tutor">Enroll Course</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 col-12 mt-4">
+						<div class="card">
+							<img src="~/assets/images/js.jpg" class="card-img-top" alt="..." />
+							<i class="bi bi-bookmark card-bookmark"></i>
+							<div class="card-body pb-0">
+								<div class="tutor-course-ratings">
+									<div class="tutor-ratings">
+										<div class="tutor-ratings">
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+										</div>
+										<div class="tutor-ratings-average">5.0</div>
+										<div class="tutor-ratings-count">(7)</div>
+									</div>
+								</div>
+								<h5 class="card-title mt-3 mb-3">Card title</h5>
+								<div class="tutor-meta mb-3">
+									<div class="d-inline">
+										<span class="tutor-meta-icon tutor-icon-user-line" area-hidden="true"><i class="bi bi-person"></i></span>
+										<span class="tutor-meta-value">815</span>
+									</div>
+
+									<div class="d-inline p-2">
+										<span class="tutor-meta-icon tutor-icon-clock-line ml-3" area-hidden="true"><i class="bi bi-clock"></i></span>
+										<span class="tutor-meta-value"> 2h 23m</span>
+									</div>
+								</div>
+
+								<div class="tutor-meta">
+									<div class="d-inline">
+										<div class="tutor-avatar d-inline">
+											<img src="~/assets/images/user-profile-icon.jpg" alt="" srcset="" />
+										</div>
+									</div>
+
+									<div class="d-inline">
+										By
+										<a href="#">Polash Mahmud</a>
+
+										In
+										<a href="#">পলাশ মাহমুদ</a>
+									</div>
+								</div>
+							</div>
+							<hr class="border-1 mb-2" />
+							<a href="#" class="btn-tutor">Enroll Course</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 col-12 mt-4">
+						<div class="card">
+							<img src="~/assets/images/js.jpg" class="card-img-top" alt="..." />
+							<i class="bi bi-bookmark card-bookmark"></i>
+							<div class="card-body pb-0">
+								<div class="tutor-course-ratings">
+									<div class="tutor-ratings">
+										<div class="tutor-ratings">
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+										</div>
+										<div class="tutor-ratings-average">5.0</div>
+										<div class="tutor-ratings-count">(7)</div>
+									</div>
+								</div>
+								<h5 class="card-title mt-3 mb-3">Card title</h5>
+								<div class="tutor-meta mb-3">
+									<div class="d-inline">
+										<span class="tutor-meta-icon tutor-icon-user-line" area-hidden="true"><i class="bi bi-person"></i></span>
+										<span class="tutor-meta-value">815</span>
+									</div>
+
+									<div class="d-inline p-2">
+										<span class="tutor-meta-icon tutor-icon-clock-line ml-3" area-hidden="true"><i class="bi bi-clock"></i></span>
+										<span class="tutor-meta-value"> 2h 23m</span>
+									</div>
+								</div>
+
+								<div class="tutor-meta">
+									<div class="d-inline">
+										<div class="tutor-avatar d-inline">
+											<img src="~/assets/images/user-profile-icon.jpg" alt="" srcset="" />
+										</div>
+									</div>
+
+									<div class="d-inline">
+										By
+										<a href="#">Polash Mahmud</a>
+
+										In
+										<a href="#">পলাশ মাহমুদ</a>
+									</div>
+								</div>
+							</div>
+							<hr class="border-1 mb-2" />
+							<a href="#" class="btn-tutor">Enroll Course</a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6 col-12 mt-4">
+						<div class="card">
+							<img src="~/assets/images/js.jpg" class="card-img-top" alt="..." />
+							<i class="bi bi-bookmark card-bookmark"></i>
+							<div class="card-body pb-0">
+								<div class="tutor-course-ratings">
+									<div class="tutor-ratings">
+										<div class="tutor-ratings">
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+											<span class="tutor-icon-star-bold"><i class="bi bi-star"></i></span>
+										</div>
+										<div class="tutor-ratings-average">5.0</div>
+										<div class="tutor-ratings-count">(7)</div>
+									</div>
+								</div>
+								<h5 class="card-title mt-3 mb-3">Card title</h5>
+								<div class="tutor-meta mb-3">
+									<div class="d-inline">
+										<span class="tutor-meta-icon tutor-icon-user-line" area-hidden="true"><i class="bi bi-person"></i></span>
+										<span class="tutor-meta-value">815</span>
+									</div>
+
+									<div class="d-inline p-2">
+										<span class="tutor-meta-icon tutor-icon-clock-line ml-3" area-hidden="true"><i class="bi bi-clock"></i></span>
+										<span class="tutor-meta-value"> 2h 23m</span>
+									</div>
+								</div>
+
+								<div class="tutor-meta">
+									<div class="d-inline">
+										<div class="tutor-avatar d-inline">
+											<img src="~/assets/images/user-profile-icon.jpg" alt="" srcset="" />
+										</div>
+									</div>
+
+									<div class="d-inline">
+										By
+										<a href="#">Polash Mahmud</a>
+
+										In
+										<a href="#">পলাশ মাহমুদ</a>
+									</div>
+								</div>
+							</div>
+							<hr class="border-1 mb-2" />
+							<a href="#" class="btn-tutor">Enroll Course</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+
 }
 </script>
