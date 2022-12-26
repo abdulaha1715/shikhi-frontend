697c5957b4819ff0484c4ec90dc40d39aa907a56
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-12-27 01:16:24 +0600

    user property

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,6 +67,10 @@ export default {
 					logout: { url: '/api/logout', method: 'post' },
 					user: { url: '/api/me', method: 'get' }
 				},
+				user: {
+				  property: 'user',
+				  // autoFetch: true
+				},
 			},
 		}
 	},
